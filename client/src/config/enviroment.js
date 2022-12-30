88f538aa8e774712d1b2ba987469d142c05ef7bc
santi-misael <santiagoalamos2000@gmail.com>
2022-12-30 00:04:37 -0300

    new back

--- client/src/config/enviroment.js
+++ client/src/config/enviroment.js
@@ -1,6 +1,7 @@
 const { REACT_APP_API_URL, REACT_APP_API_PORT } = process.env;
 // console.log(REACT_APP_API_URL, REACT_APP_API_PORT)
-export const API_URL =
-  REACT_APP_API_URL === "localhost"
-    ? `http://${REACT_APP_API_URL}:${REACT_APP_API_PORT}`
-    : `https://${REACT_APP_API_URL}`;
+export const API_URL = 
+  REACT_APP_API_URL === "localhost" ?
+  'http://localhost:3000' : 'https://alamos-individualproject-backend.onrender.com';
+  //   ? `http://${REACT_APP_API_URL}:${REACT_APP_API_PORT}`
+  //   : `https://${REACT_APP_API_URL}`;
